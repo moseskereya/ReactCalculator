9331528a12bad1f0833a1aa5b94ff87be550dfd4
Moses <lazaromoses77@gmail.com>
2020-03-26 02:13:08 +0300

    Updates

---
+++ precache-manifest.6db233bb1d74d49dd253b2453f5b37e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d2ed7bcccaa483c27163c872b1257f",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "81f940f2bc6200065910",
+    "url": "/ReactCalculator/static/css/main.22763af5.chunk.css"
+  },
+  {
+    "revision": "70a10f3bb0326e828513",
+    "url": "/ReactCalculator/static/js/2.f9bf67d2.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/ReactCalculator/static/js/2.f9bf67d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f940f2bc6200065910",
+    "url": "/ReactCalculator/static/js/main.157c35f3.chunk.js"
+  },
+  {
+    "revision": "fcb393d5ba172a59db55",
+    "url": "/ReactCalculator/static/js/runtime-main.86d3393b.js"
+  }
+]);
